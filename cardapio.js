ff2c285687e531f4f8aa92203822a9b516b95261
Biula7 <Biula0710@gmail.com>
2025-04-08 08:32:58 -0300

    Add files via upload

---
+++ cardapio.js
@@ -0,0 +1,26 @@
+
+const cardapio = [
+  {
+    categoria: "entradas",
+    nome: "Empadinha",
+    descricao: "Empada de frango com catupiry",
+    preco: "R$ 20,00",
+    imagem: "entrada.jpeg"
+  },
+  {
+    categoria: "principais",
+    nome: "Lasanha",
+    descricao: "Lasanha à bolonhesa com queijo gratinado",
+    preco: "R$ 22,00",
+    imagem: "prato_principal.jpeg"
+  },
+  {
+    categoria: "sobremesas",
+    nome: "Torta de Limão",
+    descricao: "Torta gelada com chantilly e raspas de limão",
+    preco: "R$ 22,00",
+    imagem: "torta_limao.jpeg"
+  }
+];
+
+module.exports = cardapio;
